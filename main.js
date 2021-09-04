22b8dce7d5208c4b906cade8f49b1c09ba20b204
Progress Aienobe <paienobe@gmail.com>
2021-09-04 13:33:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector(".hamburger-menu")
+const navList = document.querySelector(".nav-list")
+const navLinks = document.querySelectorAll(".nav-links")
+
+menuBtn.addEventListener("click", () => {
+    navList.classList.toggle("show-list")
+})
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navList.classList.remove("show-list")
+    })
+})
